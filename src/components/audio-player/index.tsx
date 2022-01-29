9d4c8b527b2707bf3df005bf92f55173f1a82431
Ketan Shinde <ketanshinde.pro@gmail.com>
2022-01-29 18:17:14 +0530

    audio player progress bar and current time connected to dynamic changes

--- src/components/audio-player/index.tsx
+++ src/components/audio-player/index.tsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useRef, useState } from 'react';
 import { AudioPlayerContext } from '../../providers/audio-player';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes, faPlayCircle, faPauseCircle } from '@fortawesome/free-solid-svg-icons';
+import { secondsToMinutes } from '../../helpers/utils';
 
 export interface Props { }
 
@@ -12,20 +13,26 @@ export default function CustomAudioPlayer({ }: Props) {
         audiobookData,
     } = useContext<any>(AudioPlayerContext);
 
+    const [audioDuration, setAudioDuration] = useState();
+    const [audioCurrentTime, setAudioCurrentTime] = useState(0.00);
     const [audio, setAudio] = useState();
     const [paused, setPaused] = useState(false);
 
     useEffect(() => {
-        console.log("new url");
         audio?.pause();
         setPaused(false);
         setAudio(typeof Audio !== "undefined" ? new Audio(audiobookData.fileUrl) : undefined);
-
     }, [audiobookData.fileUrl])
 
     useEffect(() => {
         audio?.load();
         audio?.play();
+        audio?.onloadedmetadata = function () {
+            setAudioDuration(audio?.duration)
+            audio?.ontimeupdate = () => {
+                setAudioCurrentTime(audio?.currentTime)
+            };
+        };
     }, [audio])
 
     return (
@@ -34,7 +41,9 @@ export default function CustomAudioPlayer({ }: Props) {
                 <div className="">
                     <button type="button"
                         className="inline-block rounded-full bg-stone-400 text-white leading-normal uppercase hover:bg-black hover:shadow-lg focus:bg-black focus:shadow-lg focus:outline-none focus:ring-0 active:bg-black active:shadow-lg transition duration-150 ease-in-out w-9 h-9"
-                        onClick={() => setIsVisible(false)}
+                        onClick={() => {
+                            setIsVisible(false)
+                        }}
                     >
                         <FontAwesomeIcon icon={faTimes} />
                     </button>
@@ -44,8 +53,8 @@ export default function CustomAudioPlayer({ }: Props) {
                         <img className="w-1/4 rounded hidden md:block" src="https://tailwindcss.com/img/card-top.jpg" alt="Album Pic" />
                     </div>
                     <div className="w-full">
-                        <h3 className="text-2xl text-grey-darkest font-medium">A Sky Full of Stars</h3>
-                        <p className="text-sm text-grey mt-1">Ghost Stories</p>
+                        <h3 className="text-2xl text-grey-darkest font-medium">{audiobookData.name}</h3>
+                        <p className="text-sm text-grey mt-1">{audiobookData.desc}</p>
                     </div>
                 </div>
                 <div className="w-full inline-flex px-3 gap-3">
@@ -66,15 +75,20 @@ export default function CustomAudioPlayer({ }: Props) {
                                 }} />
                     }
                     <div className="flex justify-between text-lg text-grey-darker">
-                        <p>0:00</p>
+                        <p>{secondsToMinutes(audioCurrentTime).toFixed(2)}</p>
                     </div>
                     <input type="range"
                         className="form-range w-4/6 h-6 p-0 focus:outline-none focus:ring-0 focus:shadow-none"
-                        defaultValue={10}
-                        onChange={(e) => { console.log("range event : ", e.target.value) }}
+                        min="0"
+                        max={audioDuration}
+                        onChange={(e) => {
+                            audio?.currentTime = e.target.value;
+                            setAudioCurrentTime(e.target.value)
+                        }}
+                        value={audioCurrentTime}
                     />
                     <div className="flex justify-between text-lg text-grey-darker">
-                        <p>4:20</p>
+                        <p>{secondsToMinutes(audioDuration).toFixed(2)}</p>
                     </div>
                 </div>
             </div>
--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,6 +1,12 @@
 /**
  * No operation (dummy function)
  */
-export const noop = () => {};
+export const noop = () => { };
 
-export const isNotNullAndUndefined = (value: any) => value !== null && value !== undefined;
+export const isNotNullAndUndefined = (value: any) =>
+  value !== null && value !== undefined;
+
+
+export const secondsToMinutes = (value: Number) => {
+  return value / 60;
+}
