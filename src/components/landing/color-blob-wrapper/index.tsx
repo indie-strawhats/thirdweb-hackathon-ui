102ef9a8eba0bcda642db4021e9f82e96fa982d6
indrajitbnikam <indrajitbnikam@gmail.com>
2022-01-30 14:13:15 +0530

    style: modify landing page

--- src/components/landing/color-blob-wrapper/index.tsx
+++ src/components/landing/color-blob-wrapper/index.tsx
@@ -13,18 +13,18 @@ const ColorBlobWrapper: FC<ColorBlobWrapperProps> = ({ children, blobSize = 'sma
       case 'small':
         return (
           <>
-            <div className="absolute top-0 w-64 h-64 bg-purple-300 rounded-full -left-2 mix4nd-multiply filter blur-xl opacity-60 animate-blob"></div>
-            <div className="absolute top-0 w-64 h-64 bg-yellow-300 rounded-full -right-2 mix4nd-multiply filter blur-xl opacity-60 animate-blob animation-delay-2000"></div>
-            <div className="absolute w-64 h-64 bg-pink-300 rounded-full -bottom-8 left-14 mix4nd-multiply filter blur-xl opacity-60 animate-blob animation-delay-4000"></div>
+            <div className="absolute top-0 w-64 h-64 bg-purple-200 rounded-full -left-2 mix4nd-multiply filter blur-xl opacity-60 animate-blob"></div>
+            <div className="absolute top-0 w-64 h-64 bg-yellow-200 rounded-full -right-2 mix4nd-multiply filter blur-xl opacity-60 animate-blob animation-delay-2000"></div>
+            <div className="absolute w-64 h-64 bg-pink-200 rounded-full -bottom-8 left-14 mix4nd-multiply filter blur-xl opacity-60 animate-blob animation-delay-4000"></div>
           </>
         );
       case 'large':
       default:
         return (
           <>
-            <div className="absolute top-0 bg-purple-300 rounded-full w-72 h-72 -left-2 mix4nd-multiply filter blur-xl opacity-70 animate-blob"></div>
-            <div className="absolute top-0 bg-yellow-300 rounded-full w-72 h-72 -right-2 mix4nd-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
-            <div className="absolute bg-pink-300 rounded-full w-72 h-72 -bottom-8 left-14 mix4nd-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+            <div className="absolute top-0 bg-purple-200 rounded-full w-96 h-96 -left-2 mix4nd-multiply filter blur-xl opacity-70 animate-blob"></div>
+            <div className="absolute top-0 bg-yellow-200 rounded-full w-96 h-96 -right-2 mix4nd-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+            <div className="absolute bg-pink-200 rounded-full w-96 h-96 -bottom-8 left-14 mix4nd-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
           </>
         );
     }
@@ -32,9 +32,9 @@ const ColorBlobWrapper: FC<ColorBlobWrapperProps> = ({ children, blobSize = 'sma
 
   return (
     <div className="w-full h-full">
-      <div className="relative w-full max-w-lg">
+      <div className="relative w-full">
         {returnBlobVariant()}
-        <div className="relative m-8">{children}</div>
+        <div className="relative w-full">{children}</div>
       </div>
     </div>
   );
